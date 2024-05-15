42cce832ef5b2e0af0110acf341f00c73d58e187
IkyMax <Iky_Max_@outlook.com>
2024-05-14 21:57:17 -0600

    Set Universal arch DMG image (Needs testing)

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -53,7 +53,7 @@ if(!isDev){
                 loggerAutoUpdater.info('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/Limbo-Studios/LimboLauncher/releases/download/v${info.version}/Limbo-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/Limbo-Studios/LimboLauncher/releases/download/v${info.version}/Limbo-Launcher-setup.dmg`
                     showUpdateUI(info)
                 }
                 
