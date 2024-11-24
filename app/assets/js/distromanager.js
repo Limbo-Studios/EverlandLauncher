81a82b3dc253ff1736f5a3699fa509ef1b700fa5
IkyMax <Iky_Max_@outlook.com>
2024-11-23 23:55:06 -0600

    Update distro

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Limbo-Studios/distro/main/LurkaryaSMP.json'
+exports.REMOTE_DISTRO_URL = 'https://resources.lsmp.site/limbo.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
